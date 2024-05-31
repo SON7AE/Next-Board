08f0aab2913522b5bbe4e4e80305f4adfbcd7d56
Marco <weartstudio@naver.com>
2024-06-01 00:22:04 +0900

    YouTube Next.js 강의 작업물 - 20240530 작업물

--- src/components/common/board/BasicBoard.tsx
+++ src/components/common/board/BasicBoard.tsx
@@ -1,41 +1,42 @@
-import { useState } from 'react'
-import { Checkbox } from '@/components/ui/checkbox'
-import { Button } from '@/components/ui/button'
-import { ChevronUp } from 'lucide-react'
-import LabelCalendar from '@/components/common/calendar/LabelCalendar'
-import MarkdownDialog from '../dialog/MarkdownDialog'
-import styles from './BasicBoard.module.scss'
+import { useEffect, useState } from "react";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { ChevronUp } from "lucide-react";
+import LabelCalendar from "@/components/common/calendar/LabelCalendar";
+import MarkdownDialog from "../dialog/MarkdownDialog";
+import styles from "./BasicBoard.module.scss";
 
-function BasicBoard() {
-    const [startDate, setStartDate] = useState<Date | undefined>()
-    const [endDate, setEndDate] = useState<Date | undefined>()
+interface Props {
+    data: {
+        title: string;
+        startDate: Date | undefined;
+        endDate: Date | undefined;
+        contents: string;
+    };
+}
 
-    const setDate = (payload: { label: string; date: Date | undefined }) => {
-        if (payload.label === 'From') setStartDate(payload.date)
-        else setEndDate(payload.date)
-    }
-
+function BasicBoard({ data }: Props) {
     return (
         <div className={styles.container}>
             <div className={styles.container__header}>
-                <div className={styles.container__header__inputBox}>
-                    <Checkbox className='w-5 h-5' />
-                    <input type='text' placeholder='Board Title Here...' className={styles.input} />
+                <div className={styles.container__header__titleBox}>
+                    <Checkbox className="w-5 h-5" />
+                    <span className={styles.title}>{data.title ? data.title : "Please enter a title for the board."}</span>
                 </div>
-                <Button variant='ghost'>
-                    <ChevronUp className='w-5 h-5 text-gray-400' />
+                <Button variant="ghost">
+                    <ChevronUp className="w-5 h-5 text-gray-400" />
                 </Button>
             </div>
             <div className={styles.container__body}>
                 <div className={styles.container__body__calendarBox}>
-                    <LabelCalendar label='From' readOnly={true} handleDate={setDate} />
-                    <LabelCalendar label='To' readOnly={true} handleDate={setDate} />
+                    <LabelCalendar label="From" data={data.startDate} readOnly={true} />
+                    <LabelCalendar label="To" data={data.endDate} readOnly={true} />
                 </div>
                 <div className={styles.container__body__buttonBox}>
-                    <Button variant='ghost' className='font-normal text-gray-400 hover:bg-green-50 hover:text-green-500'>
+                    <Button variant="ghost" className="font-normal text-gray-400 hover:bg-green-50 hover:text-green-500">
                         Duplicate
                     </Button>
-                    <Button variant='ghost' className='font-normal text-gray-400 hover:bg-red-50 hover:text-red-500'>
+                    <Button variant="ghost" className="font-normal text-gray-400 hover:bg-red-50 hover:text-red-500">
                         Delete
                     </Button>
                 </div>
@@ -44,7 +45,7 @@ function BasicBoard() {
                 <MarkdownDialog />
             </div>
         </div>
-    )
+    );
 }
 
-export default BasicBoard
+export default BasicBoard;
