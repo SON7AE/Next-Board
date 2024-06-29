221aba6ed30396d110e73742d2a9f42d4e98cfc6
Marco <weartstudio@naver.com>
2024-06-30 06:22:33 +0900

    refector: 20240629-YouTube 강의 콘텐츠 소스코드 업로드

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
-import { useRouter } from 'next/navigation'
-import { Button } from '@/components/ui/button'
-import styles from './page.module.scss'
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import styles from "./page.module.scss";
 
 function Home() {
-    const router = useRouter()
+    const router = useRouter();
 
     return (
         <div className={styles.container}>
@@ -16,12 +16,16 @@ function Home() {
                     <span>2. Add boards to page</span>
                 </div>
                 {/* 페이지 추가 버튼 */}
-                <Button variant='outline' className='w-full bg-transparent text-orange-500 border-orange-400 hover:bg-orange-50 hover:text-orange-500' onClick={() => router.push('/create')}>
+                <Button
+                    variant={"outline"}
+                    className="w-full bg-transparent text-orange-500 border-orange-400 hover:bg-orange-50 hover:text-orange-500"
+                    onClick={() => router.push("/create")}
+                >
                     Add New Page
                 </Button>
             </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
